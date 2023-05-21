a33c90a3786213f998eeb1ea62de7197951fe181
Hridhyapaul <hpdesign2021@gmail.com>
2023-05-22 00:15:46 +0600

    Fixed simple error

--- src/Section/Category/CategoryCard.jsx
+++ src/Section/Category/CategoryCard.jsx
@@ -3,7 +3,7 @@ import ReactStars from "react-rating-stars-component";
 import { Link } from 'react-router-dom';
 
 const CategoryCard = ({ toy }) => {
-    const {_id, image, name, price, rating } = toy;
+    const { _id, image, name, price, rating } = toy;
 
     const firstExample = {
         size: 24,
@@ -13,17 +13,19 @@ const CategoryCard = ({ toy }) => {
     return (
 
         <div className="card w-[380px] lg:w-[350px] bg-base-100 shadow-xl col-span-1 mx-auto">
-            <figure><img src={image} alt="Shoes" /></figure>
+            <figure><img className='h-[400px] object-cover' src={image} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+                <div className='h-[70px]'>
+                    <h2 className="card-title">{name}</h2>
+                </div>
                 <hr />
-                <div className='flex justify-between items-center'>
+                <div className='flex justify-between items-center mt-2'>
                     <p className='text-[28px] font-bold'>${price}</p>
                     <ReactStars {...firstExample} />
                 </div>
                 <div className="card-actions justify-end mt-3">
                     <Link to={`/viewDetails/${_id}`}><button className="bg-[#D268CC] px-4 py-2 text-white rounded-lg">View Details</button></Link>
-                    
+
                 </div>
             </div>
         </div>
